7ca1b13d989d6ba83174becff63910f874d3713c
emericgautier <emericgtr@gmail.com>
2022-09-10 18:39:48 +0200

    Initial commit

---
+++ cd.js
@@ -0,0 +1,4 @@
+const puppeteer = require("puppeteer");
+
+const url =
+  "https://www.cdiscount.com/informatique/achat-pc-ordinateur/megaport-pc-gamer-amd-ryzen-9-3900x-12x-3-8-ghz/f-1070853-meg4260568826719.html#mpos=0|mp";
